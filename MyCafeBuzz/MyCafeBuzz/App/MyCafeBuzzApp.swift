bd7421b882edb532db6da341245acd1fcc438d04
James McDougall <jameswilliammcdougall@gmail.com>
2024-04-17 18:58:15 -0500

    Asset upload and project setup

---
+++ MyCafeBuzz/MyCafeBuzz/App/MyCafeBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCafeBuzzApp.swift
+//  MyCafeBuzz
+//
+//  Created by James McDougall on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCafeBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyCafeBuzz/MyCafeBuzz/App/Root/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  MyCafeBuzz
+//
+//  Created by James McDougall on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MainTabView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
